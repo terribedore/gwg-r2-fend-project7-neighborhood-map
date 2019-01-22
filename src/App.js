6698956940825bc74ac2264e959a3b76828e946b
Terri Bedore <terribedore@gmail.com>
2019-01-22 13:49:42 -0600

    added Map.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Map from './component/Map.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <h1 className="App-title">Frugal Date Ideas</h1>
+        <h5 className="App-desc">Free or Cheap Places to be with Your Honey.</h5>
         </header>
+        <Map/>
       </div>
     );
   }
