e5f666c0d6380a1c8006e7c6e961652b90682289
Terri Bedore <terribedore@gmail.com>
2019-01-23 11:19:20 -0600

    adding 4square in right spot...doh

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React, { Component } from 'react';
-import './App.css';
-import Map from './components/Map.js';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './App.css'
+import Map from './components/Map.js'
 
-class App extends Component {
+
+
+var foursquare = require('react-foursquare')({
+  clientID: 'G04GDHOHNV44SCN05HBQDM0TACJMTYVR13OWDX4NJ3N3JUWL',
+  clientSecret: 'KE10VXIQUO1L2FFLOKYWDHTZOKNU23RFRFNXEMYMX1E31QM4'
+});
+
+var params = {
+  "ll": "29.424349, -98.491142",
+  "query": '',
+  "limit": 10
+};
+
+export default class FoursquareDemo extends Component {
+
+  constructor(props) {
+     super(props);
+     this.state = {
+       items: []
+     };
+   }
+
+  componentDidMount() {
+    foursquare.venues.getVenues(params)
+      .then(res=> {
+        this.setState({ items: res.response.venues });
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,9 +40,14 @@ class App extends Component {
         <h5 className="App-desc">Free or Cheap Places to be with Your Honey.</h5>
         </header>
         <Map/>
+            <div>Items:</div>
+            { this.state.items.map(item=> { return <div key={item.id}>{item.name}</div>}) }
       </div>
-    );
+  )
   }
 }
 
-export default App;
+ReactDOM.render(
+  <FoursquareDemo />,
+  document.getElementById('root')
+);
